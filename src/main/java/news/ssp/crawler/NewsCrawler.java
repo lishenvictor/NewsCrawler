d32bbc36a15d000e7b2f8c74f96eec46f84308a0
lishenvictor <383086656@qq.com>
2018-10-03 20:41:23 +0800

    修改程序

--- src/main/java/news/ssp/crawler/NewsCrawler.java
+++ src/main/java/news/ssp/crawler/NewsCrawler.java
@@ -10,6 +10,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+import net.sf.ehcache.CacheException;
+import org.apache.commons.codec.language.Caverphone1;
 import org.apache.commons.io.FileUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.ParseException;
@@ -48,11 +50,16 @@ public class NewsCrawler {
 	 * 解析主页
 	 */
 	private static void parseHomePage() {
-		while (true) {
+//		while (true) {
 			logger.info("开始爬取" + URL + "网页");
+			System.setProperty(net.sf.ehcache.CacheManager.ENABLE_SHUTDOWN_HOOK_PROPERTY, "true");
 			CacheManager cm = new CacheManager(PropertiesUtil.getValue("cacheFilePath"));
 			manager = CacheManager.getCacheManager(cm.getName());
+			if (manager == null) {
+				manager = CacheManager.create();
+			}
 			cache = manager.getCache("cnblog");
+			cache.flush();
 			CloseableHttpClient httpClient = HttpClients.createDefault(); // 获取HttpClient实例
 			HttpGet httpget = new HttpGet(URL); // 创建httpget实例
 			RequestConfig config = RequestConfig.custom().setSocketTimeout(100000) // 设置读取超时时间
@@ -107,7 +114,7 @@ public class NewsCrawler {
 			}
 			logger.info("结束爬取" + URL + "网页");
 		}
-	}
+//	}
 
 	/**
 	 * 解析首页内容 提取新闻link
@@ -124,6 +131,7 @@ public class NewsCrawler {
 			Element link = links.get(i);
 			String url = link.attr("href");
 			System.out.println(url);
+			cache.flush();
 			if (cache.get(url) != null) { // 如果缓存中存在就不插入
 				logger.info(url + "-缓存中存在");
 				continue;
@@ -222,16 +230,17 @@ public class NewsCrawler {
 			System.out.println(url);
 		}
 
-		if (imgUrlList.size() > 0) {
-			Map<String, String> replaceImgMap = downLoadImages(imgUrlList);
-			String newContent = replaceWebPageImages(content, replaceImgMap);
-			content = newContent;
-		}
+//		if (imgUrlList.size() > 0) {
+//			Map<String, String> replaceImgMap = downLoadImages(imgUrlList);
+//			String newContent = replaceWebPageImages(content, replaceImgMap);
+//			content = newContent;
+//		}
 			try {
 				String currentDatePath = DateUtil.getCurrentDatePath();
 				InputStream inputStream = new ByteArrayInputStream(getTextFromHtml(content).getBytes());
 				FileUtils.copyToFile(inputStream, new File(
-						PropertiesUtil.getValue("filePath") + currentDatePath + "/" + title.trim() + ".txt"));
+						PropertiesUtil.getValue("filePath") + currentDatePath + "/" + title.trim().replace(":","-")
+								.replace("?"," ")+ ".txt"));
 			}catch (Exception e){
 				e.printStackTrace();;
 			}
@@ -251,6 +260,7 @@ public class NewsCrawler {
 			if (pstmt.executeUpdate() == 1) {
 				logger.info(link + "-成功插入数据库");
 				cache.put(new net.sf.ehcache.Element(link, link));
+				cache.flush();
 				logger.info(link + "-已加入缓存");
 			} else {
 				logger.info(link + "-插入数据库失败");
@@ -260,99 +270,99 @@ public class NewsCrawler {
 		}
 	}
 
-	/**
-	 * 把原来的网页图片地址换成本地新的
-	 * 
-	 * @param content
-	 * @param replaceImgMap
-	 * @return
-	 */
-	private static String replaceWebPageImages(String content, Map<String, String> replaceImgMap) {
-		for (String url : replaceImgMap.keySet()) {
-			String newPath = replaceImgMap.get(url);
-			content = content.replace(url, newPath);
-		}
-		return content;
-	}
+//	/**
+//	 * 把原来的网页图片地址换成本地新的
+//	 *
+//	 * @param content
+//	 * @param replaceImgMap
+//	 * @return
+//	 */
+//	private static String replaceWebPageImages(String content, Map<String, String> replaceImgMap) {
+//		for (String url : replaceImgMap.keySet()) {
+//			String newPath = replaceImgMap.get(url);
+//			content = content.replace(url, newPath);
+//		}
+//		return content;
+//	}
 
-	/**
-	 * 下载图片到本地
-	 * 
-	 * @param imgUrlList
-	 * @return
-	 */
-	private static Map<String, String> downLoadImages(List<String> imgUrlList) {
-		Map<String, String> replaceImgMap = new HashMap<String, String>();
-
-		RequestConfig config = RequestConfig.custom().setSocketTimeout(10000) // 设置读取超时时间
-				.setConnectTimeout(5000) // 设置连接超时时间
-				.build();
-		CloseableHttpClient httpClient = HttpClients.createDefault(); // 获取HttpClient实例
-		for (int i = 0; i < imgUrlList.size(); i++) {
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-			String url = imgUrlList.get(i);
-			logger.info("开始爬取" + url + "图片");
-
-			CloseableHttpResponse response = null;
-
-			try {
-				HttpGet httpget = new HttpGet(url); // 创建httpget实例
-				httpget.setConfig(config);
-				response = httpClient.execute(httpget);
-			} catch (ClientProtocolException e) {
-				logger.error(url + "-ClientProtocolException");
-			} catch (IOException e) {
-				logger.error(url + "-IOException");
-			}
-			if (response != null) {
-				HttpEntity entity = response.getEntity(); // 获取返回实体
-				// 判断返回状态是否为200
-				if (response.getStatusLine().getStatusCode() == 200) {
-					try {
-						InputStream inputStream = entity.getContent();
-						String imageType = entity.getContentType().getValue();
-						String urlB = imageType.split("/")[1];
-						String uuid = UUID.randomUUID().toString();
-						String currentDatePath = DateUtil.getCurrentDatePath();
-						String newPath = PropertiesUtil.getValue("imagePath") + currentDatePath + "/" + uuid + "."
-								+ urlB;
-						FileUtils.copyToFile(inputStream, new File(
-								PropertiesUtil.getValue("imageFilePath") + currentDatePath + "/" + uuid + "." + urlB));
-						replaceImgMap.put(url, newPath);
-					} catch (UnsupportedOperationException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (Exception e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				} else {
-					logger.error("返回状态非200");
-				}
-			} else {
-				logger.error("连接超时");
-			}
-			try {
-				if (response != null) {
-					response.close();
-				}
-			} catch (Exception e) {
-				logger.error("Exception", e);
-			}
-			logger.info("结束爬取" + url + "图片");
-		}
-
-		return replaceImgMap;
-	}
+//	/**
+//	 * 下载图片到本地
+//	 *
+//	 * @param imgUrlList
+//	 * @return
+//	 */
+//	private static Map<String, String> downLoadImages(List<String> imgUrlList) {
+//		Map<String, String> replaceImgMap = new HashMap<String, String>();
+//
+//		RequestConfig config = RequestConfig.custom().setSocketTimeout(10000) // 设置读取超时时间
+//				.setConnectTimeout(5000) // 设置连接超时时间
+//				.build();
+//		CloseableHttpClient httpClient = HttpClients.createDefault(); // 获取HttpClient实例
+//		for (int i = 0; i < imgUrlList.size(); i++) {
+//			try {
+//				Thread.sleep(1000);
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//
+//			String url = imgUrlList.get(i);
+//			logger.info("开始爬取" + url + "图片");
+//
+//			CloseableHttpResponse response = null;
+//
+//			try {
+//				HttpGet httpget = new HttpGet(url); // 创建httpget实例
+//				httpget.setConfig(config);
+//				response = httpClient.execute(httpget);
+//			} catch (ClientProtocolException e) {
+//				logger.error(url + "-ClientProtocolException");
+//			} catch (IOException e) {
+//				logger.error(url + "-IOException");
+//			}
+//			if (response != null) {
+//				HttpEntity entity = response.getEntity(); // 获取返回实体
+//				// 判断返回状态是否为200
+//				if (response.getStatusLine().getStatusCode() == 200) {
+//					try {
+//						InputStream inputStream = entity.getContent();
+//						String imageType = entity.getContentType().getValue();
+//						String urlB = imageType.split("/")[1];
+//						String uuid = UUID.randomUUID().toString();
+//						String currentDatePath = DateUtil.getCurrentDatePath();
+//						String newPath = PropertiesUtil.getValue("imagePath") + currentDatePath + "/" + uuid + "."
+//								+ urlB;
+//						FileUtils.copyToFile(inputStream, new File(
+//								PropertiesUtil.getValue("imageFilePath") + currentDatePath + "/" + uuid + "." + urlB));
+//						replaceImgMap.put(url, newPath);
+//					} catch (UnsupportedOperationException e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					} catch (IOException e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					} catch (Exception e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					}
+//				} else {
+//					logger.error("返回状态非200");
+//				}
+//			} else {
+//				logger.error("连接超时");
+//			}
+//			try {
+//				if (response != null) {
+//					response.close();
+//				}
+//			} catch (Exception e) {
+//				logger.error("Exception", e);
+//			}
+//			logger.info("结束爬取" + url + "图片");
+//		}
+//
+//		return replaceImgMap;
+//	}
 
 	public static void start() {
 		DbUtil dbUtil = new DbUtil();
